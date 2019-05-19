d07aed9345801f633524fc5db4c5fdbc455aeb9e
alfism <alfialfarisi@gmail.com>
2019-05-20 06:59:36 +0700

    auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const router = new express.Router()
+const auth = require('../middleware/auth');
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -25,13 +26,16 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
+
+    // // /users -> all users
+    // try {
+    //     const users = await User.find({})
+    //     res.send(users)
+    // } catch (e) {
+    //     res.status(500).send()
+    // }
 })
 
 router.get('/users/:id', async (req, res) => {
