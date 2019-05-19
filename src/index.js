f3e59ef209be240a6a31d1dca32f759133177c74
alfism <alfialfarisi@gmail.com>
2019-05-19 23:04:49 +0700

    REST API (user, task)

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+// const jwt = require("jsonwebtoken");
+
+// const myFunction = () => {
+//     const token = jwt.sign({ _id:"abc123" }, 'thisisthereallife', { expiresIn: '7 days' });
+//     console.log(token);
+
+//     const data = jwt.verify(token, 'thisisthereallife');
+//     console.log(data);
+    
+    
+// };
+
+// myFunction();
