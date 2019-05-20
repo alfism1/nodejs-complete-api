255e8bcfc6be52276b73f60d6177edde4cf2cfb7
alfism <alfialfarisi@gmail.com>
2019-05-20 12:26:44 +0700

    Add logout and logout all

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,7 +11,9 @@ const auth = async (req, res, next) => {
       throw new Error();
     }
     
+    req.token = token;
     req.user = user;
+
     next();
   } catch (error) {
     res.status(401).send({"error": "Invalid token"});
--- src/routers/user.js
+++ src/routers/user.js
@@ -26,6 +26,28 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter( token => token.token!==req.token );
+        await req.user.save();
+
+        res.send({"status": "logout success"});
+    } catch (error) {
+        res.status(500).send({"status": "logout failed"});
+    }
+});
+
+router.post('/users/logoutall', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+
+        res.send({"status": "logout all success"});
+    } catch (error) {
+        res.status(500).send({"status": "logout all failed"});
+    }
+});
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 
