bbd3428d2c0f1f2ff9c811ba683c9970340ed49d
alfism <alfialfarisi@gmail.com>
2019-05-23 12:12:15 +0700

    User and task relation

--- src/index.js
+++ src/index.js
@@ -13,17 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-// const jwt = require("jsonwebtoken");
-
-// const myFunction = () => {
-//     const token = jwt.sign({ _id:"abc123" }, 'thisisthereallife', { expiresIn: '7 days' });
-//     console.log(token);
-
-//     const data = jwt.verify(token, 'thisisthereallife');
-//     console.log(data);
-    
-    
-// };
-
-// myFunction();
